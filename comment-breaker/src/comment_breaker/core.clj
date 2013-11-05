6cda0a3b9d86900bdd3faf7a32b84fdb102842a5
MoyTW <moytravis@gmail.com>
2013-11-04 19:09:23 -0800

    Initial commit. comment-breaker script.

---
+++ comment-breaker/src/comment_breaker/core.clj
@@ -0,0 +1,46 @@
+(ns comment-breaker.core
+  (:gen-class))
+(use '[clojure.tools.cli :only[cli]])
+
+(def line-length 80)
+(def sep-char "\r\n")
+
+(defn suffix-file [fstr suffix]
+  (let [splits (clojure.string/split fstr #"\.")]
+    (str (apply str (butlast splits)) suffix "." (last splits))))
+      
+(defn build-line [lines nxt]
+  (if (> (+ (count (last lines)) (inc (count nxt))) line-length)
+    (conj lines (str ";; " nxt))
+    (conj (into [] (butlast lines)) (str (last lines) \space nxt))))
+    
+(defn eat-line [line]
+  (if (and (> (count line) line-length) (.startsWith line ";;"))
+    (clojure.string/replace-first
+      (clojure.string/join sep-char
+        (reduce build-line [""] (clojure.string/split line #"\s")))
+      #"\s\;\;\s+"
+      ";;   ")
+    line))
+    
+(defn process-file [contents]
+  (clojure.string/join sep-char
+    (map eat-line (clojure.string/split contents (re-pattern sep-char)))))
+
+(defn -main
+  [& args]
+  (let [cli-vec 
+          (cli args
+               ["-i" "--in-file" "File to run comment processing on." 
+                 :default nil]
+               ["-o" "--out-file" "Output file." :default nil]
+               ["-l" "--line-length" "The desired line length." 
+                 :default 80 :parse-fn #(Integer. %)])
+        cli-map (first cli-vec)
+        ifile (:in-file (first cli-vec))
+        ofile (if-let [o (:out-file (first cli-vec))] o
+                (suffix-file ifile "-broken"))]
+    (if-not ifile (prn (last cli-vec))
+      (do
+        (def line-length (:line-length cli-map))
+        (spit ofile (process-file (slurp ifile)))))))
---
+++ comment-breaker/test/comment_breaker/core_test.clj
@@ -0,0 +1,7 @@
+(ns comment-breaker.core-test
+  (:require [clojure.test :refer :all]
+            [comment-breaker.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
