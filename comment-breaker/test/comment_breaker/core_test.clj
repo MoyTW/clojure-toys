3a1e3f46edb6b163d8f2677749b00ec7f3d15ed6
MoyTW <moytravis@gmail.com>
2014-01-11 14:06:49 -0800

    Removed comment-breaker from folder.
    Was just cluttering it up.

--- comment-breaker/test/comment_breaker/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns comment-breaker.core-test
-  (:require [clojure.test :refer :all]
-            [comment-breaker.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
