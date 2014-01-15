6558973a9529bcd580e1233f3e7516c45725a3ec
MoyTW <moytravis@gmail.com>
2014-01-14 19:54:56 -0800

    Added suggestions to landing.
    It's not terribly elegant, but I do like that it lets me easily test out
    how my extensions are working!

--- learning-ring/src/learning_ring/routes/styler.clj
+++ learning-ring/src/learning_ring/routes/styler.clj
@@ -16,17 +16,23 @@
 (html/defsnippet changes-list "landing.html" *changes-sel*
   [change]
   [:li] (html/content change))
+
+(def ^:dynamic *suggestions-sel* [:.suggestions-list :> html/first-child])
+(html/defsnippet suggestions-list "landing.html" *suggestions-sel*
+  [suggestion]
+  [:li] (html/content suggestion))
   
-(html/deftemplate landing "landing.html" [old-code new-code changes e-list]
+(html/deftemplate landing "landing.html" [old-code new-code changes suggestions e-list]
   [:.extensions] (html/content (map #(extensions-list %) e-list))
   [:.changes-list] (html/content (map #(changes-list %) changes))
+  [:.suggestions-list] (html/content (map #(suggestions-list %) suggestions))
   [:.code-input] (html/content old-code)
   [:.code-output] (html/content new-code))
 
 (defn resolve-landing [code]
   (let [result (f/apply-all-extensions (par/parser code))]
-    (response (apply str (landing code (:text result) (:changes result) f/extensions)))))
+    (response (apply str (landing code (:text result) (:changes result) (:suggestions result) f/extensions)))))
   
 (compojure/defroutes routes
-  (compojure/GET "/styler" [] (response (apply str (landing "" "" [] f/extensions))))
+  (compojure/GET "/styler" [] (response (apply str (landing "" "" [] [] f/extensions))))
   (compojure/POST "/styler" {{code "code"} :params} (resolve-landing code)))
