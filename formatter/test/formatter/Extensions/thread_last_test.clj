6e706d992a9b2f809950f8d99e7108e7391a09af
MoyTW <moytravis@gmail.com>
2014-01-21 18:02:23 -0800

    Tests for thread-last.
    Updated thread-last tests to conform to the four following criteria as per
    this blog post: http://www.reflectionlibrary.co/blog/?p=157
    	* Depth of nesting greater than a given level
    	* Nesting depth for non-last args less than a given level
    	* The call is a top-level threadable call
    	* The call is to a function for which treading makes sense

--- formatter/test/formatter/Extensions/thread_last_test.clj
+++ formatter/test/formatter/Extensions/thread_last_test.clj
@@ -18,6 +18,7 @@
     (is (= 1
            (count (get-suggestions clojuredocs-example))))))
 
+;;;; Using styler code in Resources:
 (def code-styler (slurp "test/resources/styler_20140115.txt"))
 (def styler-suggestions (get-suggestions code-styler))
 (def first-suggestions (set-of-first styler-suggestions))
@@ -43,21 +44,45 @@
     (is 
       (contains? first-suggestions "(compojure/GET"))))
 
+;;;; WHY DO WE EVEN WANT TO PICK THIS ONE UP!?
+;;;; It's not threadable in the least!
+;;;; Changed it to *does not* pick it up.
 ;; Tests for:
 ;(html/defsnippet extensions-list "landing.html" *extensions-sel*
 ;  [extension]
 ;  [:a] (html/do->
 ;         (html/content (:description extension))
 ;         (html/set-attr :href (:url extension))))
-; There are other defsnippets that could *possibly* be picked up if code is wrong! It should only pick up this one, though.
 (deftest html-defsnippet
-  (testing "Tests that it picks up html/defsnippet (level 4 nesting)."
+  (testing "Tests that it *doens't* pick up html/defsnippet (L 3 non-last)."
     (is
-      (= 1
+      (= 0
          (->> styler-suggestions
               (map #(first (clojure.string/split % #" ")))
               (filter #(= "(html/defsnippet" %))
               (count))))))
+
+;;;; Using split-chain
+(def split-chain "(+ 1 (- 2 (- 3 (+ 4 (- 5 6))) (+ 7 (- 8 (+ 9 (+ 10 11))))))")
+(def split-suggestions (into #{} (get-suggestions split-chain)))
+
+(deftest does-not-suggest-multipath-calls
+  (testing "Tests that it doesn't suggest calls which have split paths"
+    (is (not (contains? split-suggestions split-chain)))))
+
+(deftest suggests-subtrees
+  (testing "Tests that it does suggest subtrees inside a non-threading node"
+    (is (and (contains? split-suggestions "(- 3 (+ 4 (- 5 6)))")
+             (contains? split-suggestions "(+ 7 (- 8 (+ 9 (+ 10 11))))")))))
+
+;; Tests that if there's a long thread-tail, it only picks the highest-order thread
+; So, (+ 1 (- 2 (- 3 (+ 4 (- 5 (+ 6 (+ 7 (- 8 (+ 9 (+ 10)))))))))) should only 
+; return one result! It should not return, say, (- 8 (+ 9 (+ 10))).
+(def long-chain "(+ 1 (- 2 (- 3 (+ 4 (- 5 (+ 6 (+ 7 (- 8 (+ 9 10)))))))))")
+(deftest does-not-suggest-subresults
+  (testing "Tests that it doesn't suggest sub-results."
+    (is (= 1
+           (count (get-suggestions long-chain))))))
 
 ;; Test to see whether we can rebind *do-not-nest*
 (binding [ext/*do-not-nest* (conj ext/*do-not-nest* "html/defsnippet")]
