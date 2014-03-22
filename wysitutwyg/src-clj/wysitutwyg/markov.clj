3b1d44b245fe0c0021c0261c786406aa76d7a617
MoyTW <moytravis@gmail.com>
2014-03-21 17:47:29 -0700

    Code to parse corpus->json on server
    markov.clj contains code to parse the corpus server-side, then translate
    it to json. It outputs the json as an array of key/value arrays instead of
    a map to make serialization/deserialization simple - you can use #(into {}
    %) to easily transform the corpus counts to a map.

--- wysitutwyg/src-clj/wysitutwyg/markov.clj
+++ wysitutwyg/src-clj/wysitutwyg/markov.clj
@@ -8,7 +8,9 @@
  
 (defn parse-counts
   "Transforms the corpus into a mapping of n-grams to counts, of the form:
-  {['He' 'is'] {'a' 2}, ['is' 'a'] {'sad' 2, 'mad' 2}}"
+  [[['He' 'is'] {'a' 2}], [['is' 'a'] {'sad' 2, 'mad' 2}]]. The output is a
+  vector of key/value vectors for easy json serialization, which may be easily
+  converted back through #(into {} %)."
   [n corpus-string]
   (loop [counts {}
          corpus (filter (comp not-delimiter? first)
@@ -19,17 +21,28 @@
           (recur (update-in counts [state follows] (fnil inc 0)) (rest corpus))
           counts))))
 
-(defn a-s [s] (vec (map #(apply str %) s)))
+(defn keys-to-strings [s]
+  (vec (map #(apply str %) s)))
+
+(defn inner-map [m]
+  (into {} (map (fn [[k v]] [(apply str k) v]) m)))
 
 (defn stringify
   [m]
-  (into {} (map #(update-in % [0] a-s) m)))
+  (->> m 
+       (map #(update-in % [0] keys-to-strings))
+       (map #(update-in % [1] inner-map))))
 
 (defn parse-and-save
   [infile outfile n]
   (with-open [w (clojure.java.io/writer outfile)]
-    ;(json/write (stringify (parse-counts n "The quick brown fox jumped")) w)))
     (spit outfile (json/write-str (stringify (parse-counts n (slurp infile)))))))
-  
-(parse-and-save "F:/aP-Personal_Projects/Clojure/wysitutwyg/resources/public/corpora/sonnets/corpus.txt"
-"F:/aP-Personal_Projects/Clojure/wysitutwyg/resources/public/corpora/sonnets/onegram.json" 1)
+ 
+(parse-and-save 
+  "resources/public/corpora/sonnets/corpus.txt" 
+  "resources/public/corpora/sonnets/onegram.json" 
+  1)
+(parse-and-save 
+  "resources/public/corpora/sonnets/corpus.txt" 
+  "resources/public/corpora/sonnets/twogram.json" 
+  2)
--- wysitutwyg/src-cljs/wysitutwyg/hello.cljs
+++ wysitutwyg/src-cljs/wysitutwyg/hello.cljs
@@ -4,16 +4,19 @@
             [clojure.browser.net :as net]
             [goog.net.XhrIo :as xhrio]))
 
-(def interval 2000)
+(def interval 1000)
 
 (def self-url (str (.-protocol (.-location js/window)) "//" (.-host (.-location js/window))))
 
 (def corpus-counts (atom ""))
 
 ;; bah! This parsing action takes too long. Parse it on the server, and provide it as a resource!
-(defn set-corpus-counts [corpus]
+#_(defn set-corpus-counts [corpus]
   (reset! corpus-counts (markov/parse-counts 2 corpus)))
 
+(defn set-corpus-counts [corpus]
+  (reset! corpus-counts corpus))
+  
 (defn by-id
   "Utility function for getElementById."
   [id]
@@ -26,6 +29,12 @@
     (set! (.-value (by-id "outputtext")) output)))
 
 ;; Retrieves the text of the corpus from the server
-(xhrio/send (str self-url "/res/corpora/sonnets.txt") #(set-corpus-counts (.getResponseText (.-target %))))
+;; TODO: Hardcoded test file should be choosable!
+(xhrio/send (str self-url "/res/corpora/sonnets/onegram.json") 
+            #(reset! corpus-counts 
+                     (->> (.-target %) 
+                          (.getResponseJson) 
+                          (js->clj) 
+                          (into {}))))
 
 (js/setInterval #(create-sonnet @corpus-counts) interval)
