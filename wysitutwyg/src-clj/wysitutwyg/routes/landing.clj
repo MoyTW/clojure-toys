de79a90ef322731571a319e49a958358abd32b9e
MoyTW <moytravis@gmail.com>
2014-03-24 18:44:28 -0700

    Adding in html allowing corpus switching.
    None of the backing is in, it's just some visuals to declare intentions
    and such.

--- wysitutwyg/src-clj/wysitutwyg/routes/landing.clj
+++ wysitutwyg/src-clj/wysitutwyg/routes/landing.clj
@@ -3,8 +3,18 @@
             [compojure.core :as compojure])
   (:use ring.util.response))
 
-(html/deftemplate landing "pages/landing.html" []
-  [:head] (html/append (first (html/html [:script {:src "res/js/main.js"}]))))
+;; This should correspond to actual, well, corpora.
+(def my-corpora 
+  [{:name "Shakespeare", :location "sfldr"} 
+   {:name "Random Text", :location "rtext"} 
+   {:name "XKCD", :location "xkcdtext"}])
+
+(html/deftemplate landing "pages/landing.html" [corpora]
+  [:head] (html/append (first (html/html [:script {:src "res/js/main.js"}])))
+  [:select#corpus-selection :option]
+    (html/clone-for [corpus corpora] 
+                    [:option] (html/content (corpus :name))
+                    [:option] (html/set-attr :value (corpus :location))))
 
 (compojure/defroutes routes
-  (compojure/GET "/" [] (landing)))
+  (compojure/GET "/" [] (landing my-corpora)))
