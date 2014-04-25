779b07b906c31f55013dc020c1c9ccc36b8c420d
MoyTW <moytravis@gmail.com>
2014-04-24 20:58:05 -0700

    Retooled how parsing works.
    Runner contains code used for parsing. It scans the corpora directory,
    searching for parses.json files in the child directories.
    
    Each parser.json file defines one or more desired parses with parameters
    (arity, delimiters, end nodes, output file) and also contains a reference
    to the source text to be parsed. The parsing code creates one output parse
    for each parse request.

--- wysitutwyg/src-clj/wysitutwyg/markov/parser.clj
+++ wysitutwyg/src-clj/wysitutwyg/markov/parser.clj
@@ -1,11 +1,6 @@
-(ns wysitutwyg.markov.parser
-  (:require [clojure.data.json :as json]))
+(ns wysitutwyg.markov.parser)
 
 (def test-text "Me is mad! It is mad? She is mad. He is sad! It is sad? She is sad. Ze mad? Ze sad? Ze jelly? Ha! He is mad! He is mad!")
-
-(def delimiter-regex " ")
-(def end-strings #{"." "!" "?"})
-; How to handle ellipses?
 
 ;; I won't be winning any elegant code competitions...
 (defn split-by-string
@@ -17,7 +12,7 @@
                 pre (if-let [p (seq (.substring string 0 idx))] [p])
                 mid (if-let [m (seq (.substring string idx e-idx))] [m])
                 suf (seq (.substring string e-idx))]
-          (recur (apply str suf) (concat out pre mid)))
+            (recur (apply str suf) (concat out pre mid)))
           (if (empty? string) 
               (vec (map #(apply str %) (vec out)))
               (conj (vec (map #(apply str %) out)) string))))))
@@ -30,14 +25,13 @@
   [string splitters]
   (reduce split-step (split-by-string string (first splitters)) (rest splitters)))
 
-(defn process-corpus [corpus]
+(defn process-corpus [corpus {:keys [delimiter-regex end-strings]}]
   "Processes the corpus into a collection of segments, determined by the
   delimiter set. Members of end-strings are grouped into their own segments."  
   (->> (.split corpus delimiter-regex)
        (vec) ; If there are no splits, String.split produces [corpus].
        (map #(apply str %))
        (mapcat #(split-by-all % end-strings))))
-(prn (process-corpus test-text))
 
 (defn keys-to-strings [s]
   (vec (map #(apply str %) s)))
@@ -63,7 +57,7 @@
             start))))
 
 (defn build-output-map
-  [start counts]
+  [start counts end-strings]
   {:start (map #(update-in % [0] keys-to-strings) start)
    :end (map str end-strings)
    :counts (stringify counts)})
@@ -77,22 +71,23 @@
   COUNTn is an int
   NEXTn := [STATEn {STR0 COUNT0, STRn, COUNTn}]
   This isn't really good documentation; it'll confuse people who aren't me."
-  [n counts start corpus]
-  (let [[state rest-corpus] (split-at n corpus)
-         follows (first rest-corpus)]
-    (cond
-      (and (seq rest-corpus) (end-strings (last state)))
-        (recur n
-               (update-in counts [state follows] (fnil inc 0))
-               (update-start start n follows rest-corpus)
-               (rest corpus))
-      (seq rest-corpus)
-        (recur n
-               (update-in counts [state follows] (fnil inc 0))
-               start
-               (rest corpus))
-      :else
-        (build-output-map start counts))))
+  [corpus n end-strings]
+  (loop [counts {} 
+         start (update-start {} n (first corpus) corpus)
+         corpus corpus]
+    (let [[state rest-corpus] (split-at n corpus)
+           follows (first rest-corpus)]
+      (cond
+        (and (seq rest-corpus) (end-strings (last state)))
+          (recur (update-in counts [state follows] (fnil inc 0))
+                 (update-start start n follows rest-corpus)
+                 (rest corpus))
+        (seq rest-corpus)
+          (recur (update-in counts [state follows] (fnil inc 0))
+                 start
+                 (rest corpus))
+        :else
+          (build-output-map start counts end-strings)))))
 
 ;; Does not properly generate :start nodes for counts > 1!
 ;; Actually, it doesn't really know how to handle counts > 1 at all...hmm.
@@ -105,63 +100,15 @@
     start-state | ((\\w \\o \\r \\d \\1) (\\w \\2))
     end-state | (\\w \\o \\r \\d)
   with the start and end states mapped to :start and :end."
-  [n corpus-string]
-  (let [processed (process-corpus corpus-string)]
-    (parse-step n
-                {}
-                (update-start {} n (first processed) processed)
-                processed)))
+  [corpus {:keys [arity end-strings delimiter-regex] :as pattrs}]
+  (let [processed (process-corpus corpus pattrs)]
+    (parse-step processed
+                arity
+                end-strings)))
 
-(defn parse-and-save
-  [infile outfile n]
-  (with-open [w (clojure.java.io/writer outfile)]
-    (spit outfile (json/write-str (parse-counts n (slurp infile))))))
-    
-(defn do-parses []
-  (do
-    (parse-and-save
-      "resources/public/corpora/test/test.txt"
-      "resources/public/corpora/test/test1.json"
-      1)
-    (parse-and-save
-      "resources/public/corpora/test/repeating.txt"
-      "resources/public/corpora/test/repeating1.json"
-      1)
-    (parse-and-save 
-      "resources/public/corpora/sonnets/corpus.txt" 
-      "resources/public/corpora/sonnets/1.json" 
-      1)
-    (parse-and-save 
-      "resources/public/corpora/sonnets/corpus.txt" 
-      "resources/public/corpora/sonnets/2.json" 
-      2)
-    (parse-and-save 
-      "resources/public/corpora/sonnets/corpus.txt" 
-      "resources/public/corpora/sonnets/3.json" 
-      3)
-    (parse-and-save 
-      "resources/public/corpora/loremipsum/corpus.txt" 
-      "resources/public/corpora/loremipsum/1.json" 
-      1)
-    (parse-and-save 
-      "resources/public/corpora/loremipsum/corpus.txt" 
-      "resources/public/corpora/loremipsum/2.json" 
-      2)
-    (parse-and-save 
-      "resources/public/corpora/loremipsum/corpus.txt" 
-      "resources/public/corpora/loremipsum/3.json" 
-      3)))
-#_(do-parses)
-#_(parse-and-save
-  "resources/public/corpora/test/test.txt"
-  "resources/public/corpora/test/test1.json"
-  1)
-(def end-strings #{"\n"})
-(parse-and-save
-  "resources/public/corpora/sonnets/short.txt"
-  "resources/public/corpora/sonnets/short1.json"
-  1)
-(parse-and-save
-  "resources/public/corpora/sonnets/corpus.txt"
-  "resources/public/corpora/sonnets/1.json"
-  1)
+(prn (parse-counts test-text {:arity 1 :end-strings #{"." "!" "?"} :delimiter-regex " "}))
+
+(def expected-output
+{:start '([["Ha"] 1] [["Ze"] 3] [["He"] 3] [["She"] 2] [["It"] 2] [["Me"] 1]), :end '("!" "." "?"), :counts '([["!"] {"He" 2, "It" 2}] [["is"] {"sad" 3, "mad" 5}] [["It"] {"is" 2}] [["Ze"] {"jelly" 1, "sad" 1, "mad" 1}] [["."] {"Ze" 1, "He" 1}] [["She"] {"is" 2}] [["mad"] {"." 1, "?" 2, "!" 3}] [["sad"] {"." 1, "?" 2, "!" 1}] [["Me"] {"is" 1}] [["Ha"] {"!" 1}] [["He"] {"is" 3}] [["jelly"] {"?" 1}] [["?"] {"Ha" 1, "Ze" 2, "She" 2}])})
+
+(assert (= expected-output (parse-counts test-text {:arity 1 :end-strings #{"." "!" "?"} :delimiter-regex " "})))
---
+++ wysitutwyg/src-clj/wysitutwyg/markov/runner.clj
@@ -0,0 +1,91 @@
+(ns wysitutwyg.markov.runner
+  (:import java.io.File)
+  (:require [clojure.data.json :as json]
+            [wysitutwyg.markov.parser :as parser]))
+
+;;; Assumes a file structure of the following configuration:
+;;; root
+;;;  |-> corpus_1
+;;;       |-> corpus.txt
+;;;       |-> parses.json
+;;;  |-> arbitrary_directory_name
+;;;       |-> arbitrary_corpus_name.txt
+;;;       |-> parses.json
+;;;  |-> corpus_n
+;;;       |-> corpus_n.txt
+;;;       |-> parses.json
+;;; where parses.json is a json file of the following form:
+;;;   {"targets":
+;;;    [{"arity":1,
+;;;      "end-strings":[".", "!", "?"],
+;;;      "delimiter-regex":" ",
+;;;      "outfile":"1.json"},
+;;;     {"arity":2,
+;;;      "end-strings":[".", "!", "?"],
+;;;      "delimiter-regex":" ",
+;;;      "outfile":"2.json"}],
+;;;    "corpus":"corpus.txt"}
+;;; with each member of the "targets" array representing a request that the
+;;; corpus be parsed to the destination, including parsing parameters.
+
+(defn read-json-str
+  "Function to map from json to a Clojure map. Keywordizes, places end-strings
+  into a set."
+  [string]
+  (json/read-str string
+                 :key-fn keyword
+                 :value-fn #(if (= %1 :end-strings) (into #{} %2) %2)))
+                 ; I bet they built something in for this that I'm not using.
+
+(defn get-dirs
+  "Returns a sequence of File objects representing directories from the 
+  indicated directory."
+  [dir]
+  {:pre (= (type dir) java.io.File)}
+  (->> dir
+       (.listFiles)
+       (filter #(.isDirectory %))))
+
+(defn find-file
+  "Given a directory, finds the file with a matching name, or empty seq."
+  [dir name]
+  {:pre (= (type dir) java.io.File)}
+  (->> dir
+       (.listFiles)
+       (filter #(= name (.getName %)))))
+
+(defn resolve-parse
+  "Resolves a single parse request, outputting to filesystem."
+  [corpus path {:keys [outfile] :as parse}]
+  (prn (str "    Executing parse, writing to: " outfile))
+  (with-open [w (clojure.java.io/writer (str path File/separator outfile))]
+    (spit w (json/write-str (parser/parse-counts corpus parse)))))
+
+(defn resolve-parses
+  "Takes a map of requests and parses and outputs all results."
+  [{:keys [path targets corpus]}]
+  {:pre (= (type path) java.io.File)}
+  (prn (str "Parsing in folder " path ":"))
+  (prn "Target File: " corpus)
+  (let [corpus-text (slurp (str path File/separator corpus))]
+    (doall (map #(resolve-parse corpus-text path %) targets))))
+
+(defn run-all-parses
+  "Finds all parses.json files in subdirectories of path, and resolves the
+  parse requests according to their contents."
+  [path name]
+  {:pre [(or (= (type path) java.lang.String) (= (type path) java.io.File))]}
+  (->> (if (= java.lang.String (type path)) 
+           (File. path)
+           path)
+       (get-dirs)
+       (mapcat #(find-file % name))
+       (map #(assoc (read-json-str (slurp %))
+                    :path
+                    (.getParentFile %)))
+       (map resolve-parses)
+       (dorun)))
+
+(def corpora-path "resources/public/corpora")
+(def json-name "parses.json")
+(run-all-parses corpora-path json-name)
