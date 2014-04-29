44cf91a76004c6ade6ee9e336f81733490944c2c
MoyTW <moytravis@gmail.com>
2014-04-28 22:00:17 -0700

    Rewriting my dfs code, completely.
    My DFS code is terrible! It is horrible, terrible, no good very bad. The
    issue is that if you look closely at what the "queue" was doing, it was
    storing the entire sequence up to that point, basically walking down the
    tree expanding one level out from a strict dfs.
    
    I'm rewriting it to be a better, less memory-intensive dfs. Not that
    it's strictly necessary (I mean, I'm not hurting for memory or anything)
    but The Algorithm Is Totally Wrong!

--- wysitutwyg/src-clj/wysitutwyg/markov/builder.clj
+++ wysitutwyg/src-clj/wysitutwyg/markov/builder.clj
@@ -3,10 +3,15 @@
 
 ;; Fixes seed for reproducability
 (def ^:dynamic ^java.util.Random *rnd* (java.util.Random. 1))
-(defn get-rand [x]
+(defn- get-rand [x]
   (.nextInt *rnd* x))
-        
-(defn pick-word
+
+(defmacro with-seed
+  [seed & code]
+  `(binding [*rnd* (java.util.Random. ~seed)]
+    ~@code))
+
+(defn- pick-word
   "Takes a map of counts in the form of {'sad' 2, 'mad' 3} (values from the 
   n-gram map) and picks a word, by the ratio of the counts."
   [words]
@@ -19,7 +24,7 @@
             (nth keys i)
             (recur (inc i) (+ (counts i) sum)))))))
 
-(defn pick-words
+(defn- pick-words
   "Returns a lazy sequence of the next states from the start state, generated
   by repeatedly picking words."
   [counts]
@@ -29,18 +34,18 @@
               nil))]
     (lazy-seq (step counts))))
 
-(defn find-first-end
+(defn- find-first-end
   "Returns the first end node following the specified state, nil if none."
   [state {:keys [counts end] :as datamap}]
   (first (filter end (pick-words (counts state)))))
 
-(defn words-to-states
+(defn- words-to-states
   "Takes a state and a collection of words, and maps it to the next set of
   states."
   [state words]
   (map #(conj (vec (rest state)) %) words))
 
-(defn gen-next-continuations
+(defn- gen-next-continuations
   [{:keys [counts end] :as datamap} {:keys [depth queue state]}]
   (->> (counts state)
        (pick-words)
@@ -49,12 +54,12 @@
        (map #(hash-map :depth (inc depth)
                        :queue (conj queue state)
                        :state %1))))
-  
+
 ;; cont consists of: {:depth depth, :queue string, :state state}
 ;;   Works for 2-3 tuples with caveats - many fewer viable paths, so drops to
 ;; dive a lot more! Also - it has n too many words, where n is one less than
 ;; the size of the tuple.
-(defn dfs-cont
+(defn- dfs-cont
   [{:keys [counts end] :as datamap} n continuations]
   (if-let [{:keys [depth queue state] :as cont} (first continuations)]
     (if (>= (+ depth (count state)) n)
@@ -65,7 +70,37 @@
           (recur datamap n (apply conj (rest continuations) new-conts))
           (recur datamap n (rest continuations))))))
 
-(defn dfs-begin
+(defn gen-next-info
+  [{:keys [counts end] :as datamap} {:keys [depth node edges]}]
+  (let [next-node (conj (vec (rest node)) (first edges))]
+    {:depth (inc depth)
+     :node next-node
+     :edges (->> (counts next-node)
+                 (pick-words))}))
+
+;; More true implementation of dfs
+(defn dfs-true
+  [{:keys [counts end] :as datamap} n stack]
+  (if-let [{:keys [depth node edges] :as node-info} (first stack)]
+    (cond
+      (and (= (+ depth (dec (count node))) n) ; TODO: lol
+           (end (last node)))
+        (reverse (map #(% :node) stack))
+      (or (= depth n) (empty? edges)) (recur datamap n (rest stack))
+      :else
+        (let [next-info (gen-next-info datamap node-info)
+              new-node-info (update-in node-info [:edges] rest)]
+          (recur datamap n (conj (cons new-node-info (rest stack)) next-info))))))
+
+(defn- dfs-true-begin
+  [{:keys [start counts] :as datamap} n]
+  {:pre [(pos? n)]}
+  (->> (pick-words start)
+       (map #(dfs-true datamap n [{:depth 0 :node % :edges (pick-words (counts %))}]))
+       (remove nil?)
+       (first)))
+
+(defn- dfs-begin
   [{:keys [start counts] :as datamap} n]
   {:pre [(pos? n)]}
   (->> (pick-words start)
@@ -74,16 +109,17 @@
        (first)))
 
 ;; Tail-recursive
-(defn dive
+(defn- dive
   [state {:keys [counts] :as datamap} n depth cont]
   (if (>= (+ depth (count state)) n)
       (conj cont state)
       (let [result (conj (vec (rest state)) (pick-word (counts state)))]
         (recur result datamap n (inc depth) (conj cont state)))))
 
-(defn dfs-single
+(defn- dfs-single
   [{:keys [start counts] :as datamap} n]
   {:pre [(pos? n)]}
+  (prn "Diving!")
   (dive (pick-word start) datamap n 0 []))
 
 (defn gen-sentence
@@ -97,3 +133,44 @@
          (reduce #(if (end %2) 
                       (str %1 %2)
                       (str %1 \space %2))))))
+
+(defn gen-sentence-true-dfs
+  [{:keys [end] :as datamap} n]
+  {:pre [(pos? n)]}
+  (let [coll (if-let [s (dfs-true-begin datamap n)] s (dfs-single datamap n))]
+    (->> coll
+         (rest)
+         (map last)
+         (concat (first coll))
+         (reduce #(if (end %2) 
+                      (str %1 %2)
+                      (str %1 \space %2))))))
+
+(def map-loc "resources/public/corpora/loremipsum/2.json")
+(use 'wysitutwyg.markov.textgen)
+(def dmap (read-into-datamap map-loc))
+
+;; TODO: Okay, it doesn't generate the currect length if you have 2-tuples.
+;; I thought I fixed that, but I didn't.
+;; This is why tests are good.
+
+(defn test-both [n]
+  (do
+    (prn "LENGTH: " n)
+    (binding [*rnd* (java.util.Random. 1)]
+      (prn "Psdo: " (gen-sentence dmap n)))
+    (binding [*rnd* (java.util.Random. 1)]
+      (prn "True: " (gen-sentence-true-dfs dmap n)))))
+
+; (test-both 2)
+(test-both 10) ; lol that tricked me into thinking it was length 9!
+;;; TODO: Issue: punctuation has its own depth in dives
+(test-both 20)
+(test-both 40) ;; Issue: 39 not 40!
+(test-both 30) ;; Issue: 39 not 40!
+
+#_(binding [*rnd* (java.util.Random. 1)]
+  (prn (gen-sentence dmap 500)))
+
+#_(binding [*rnd* (java.util.Random. 1)]
+  (prn (gen-sentence-true-dfs dmap 500)))
