2f515802d8088cad6851c98cae86f10941aa84f4
MoyTW <moytravis@gmail.com>
2014-05-20 21:37:00 -0700

    Readded ability to select your corpus.
    Also removed the button to change corpus; it now dispatches immediately
    after the user changes the selection.
    
    The "File" field still does nothing, however.

--- wysitutwyg/src-clj/wysitutwyg/routes/landing.clj
+++ wysitutwyg/src-clj/wysitutwyg/routes/landing.clj
@@ -4,16 +4,16 @@
   (:use ring.util.response))
 
 ;; This should correspond to actual, well, corpora.
-(def my-corpora 
-  [{:name "Shakespeare's Sonnets", :location "res/corpora/sonnets/"} 
-   {:name "Translation of Lorem Ipsum", :location "res/corpora/loremipsum/"}])
+(def my-corpora
+  [{:name "Shakespeare's Sonnets", :value "sonnets"} 
+   {:name "Translation of Lorem Ipsum", :value "loremipsum"}])
 
 (html/deftemplate landing "pages/landing.html" [corpora]
   [:head] (html/append (first (html/html [:script {:src "res/js/main.js"}])))
   [:select#corpus-selection :option]
     (html/clone-for [corpus corpora]
                     [:option] (html/content (corpus :name))
-                    [:option] (html/set-attr :value (corpus :location)))
+                    [:option] (html/set-attr :value (corpus :value)))
   [:select#corpus-selection [:option html/first-of-type]] (html/set-attr :selected "selected"))
 
 (compojure/defroutes routes
--- wysitutwyg/src-cljs/wysitutwyg/hello.cljs
+++ wysitutwyg/src-cljs/wysitutwyg/hello.cljs
@@ -6,42 +6,67 @@
             [goog.structs :as structs]
             [goog.Uri.QueryData :as query]))
 
+;;; Find the URL of the page
 (def self-url (str (.-protocol (.-location js/window)) 
                    "//"
                    (.-host (.-location js/window))))
 
-;{"corpus":"sonnets"
-; "file":"1.json"
-; "body":"foo bar baz"}
-            
-;;;; Transformer
-(def transform-url (str self-url "/transformer"))
+(def input-name "inputtext")
+(def output-name "outputtext")
+(def corpus-options-name "corpus-selection")
 
+(def transform-url (str self-url "/transformer"))
 (def xhr (net/xhr-connection))
-(def test-post-clojure
-  "{\"corpus\":\"sonnets\"
-    \"file\":\"1.json\"
-    \"body\":\"foo bar baz z z z z\"}")
+
+;;;; Utility
+(defn by-id
+  "Utility function for getElementById."
+  [id]
+  (.getElementById js/document id))
+
+(defn to-json
+  "Converts clojure data to json using JSON/stringify."
+  [data]
+  (JSON/stringify (clj->js data)))
+
+;;;; Transformer
 (def headers
   (-> {:Content-Type "application/json"}
       clj->js
       structs/Map.))
 
+(defn build-request-body
+  "Polls the HTML elements and builds a request from them."
+  []
+  (to-json
+    {:corpus (.-value (by-id corpus-options-name))
+     :file "1.json" ; TODO: Allow user to select
+     :body (.-value (by-id input-name))}))
+
 (defn callback
   [reply]
   (let [target (.-target reply)]
-    (js/alert (str (.getLastError target) (.getStatus target) (.isSuccess target) (.getResponseText target)))))
+    (if-not (.isSuccess target)
+            (js/alert (str (.getLastError target) (.getStatus target)))
+            (set! (.-innerHTML (by-id output-name)) 
+                  (.getResponseText target)))))
 
-(defn get-json []
-  #_(xhrio/send transform-url callback)
-  #_(xhrio/send transform-url callback "POST")
-  (xhrio/send transform-url callback "POST" test-post-clojure headers))
+(defn request-text []
+  (xhrio/send transform-url callback "POST" (build-request-body) headers))
 
+;;;; Handle corpus change event
+(defn onchange-corpus 
+  "Refreshes the text."
+  []
+  (request-text))
+  
 ;;;; Timers
 (def typingInterval 1000)
 (def timer nil)
 
-(defn start-timer
+(defn oninput-event
+  "Clears the timer, if exists, and restarts it. Calls request-text when the
+  timer expires."
   []
   (js/clearTimeout timer)
-  (def timer (js/setTimeout get-json typingInterval)))
+  (def timer (js/setTimeout request-text typingInterval)))
