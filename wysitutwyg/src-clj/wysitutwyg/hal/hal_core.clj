17d1ded07fb135f6de858fa27537747da36ef763
MoyTW <moytravis@gmail.com>
2014-05-23 21:43:39 -0700

    Building up corpora+filling in hal.

--- wysitutwyg/src-clj/wysitutwyg/hal/hal_core.clj
+++ wysitutwyg/src-clj/wysitutwyg/hal/hal_core.clj
@@ -8,6 +8,10 @@
   #{:href :templated :type :deprecation :name :profile :title :hreflang})
 (def reserved-resource-properties
   #{:_links :_embedded})
+  
+(defn is-resource
+  [resource]
+  (-> resource :_links :self :href))
 
 (defn new-resource
   [self]
@@ -18,7 +22,8 @@
   this to links. The link relation 'curies' is reserved, and cannot be 
   used."
   [resource rel href & property-pairs]
-  {:pre [(not= (keyword rel) :curies) ; "curies" is reserved
+  {:pre [(is-resource resource)
+         (not= (keyword rel) :curies) ; "curies" is reserved
          (every? link-properties (map keyword (take-nth 2 property-pairs)))
          (even? (count property-pairs))]}
   (let [link {(keyword rel) 
@@ -28,9 +33,11 @@
 (defn add-multi-link
   "Adds an array of links associated with one rel. The link relation 'curies'
   is reserved, and cannot be used. Links are maps from link-properties to 
-  values."
+  values. Use like: (add-multi-link resource rel {:href 'www.google.com'}) -
+  probably going to change the sig later though."
   [resource rel & links]
-  {:pre [(not= (keyword rel) :curies) ; "curies" is reserved
+  {:pre [(is-resource resource)
+         (not= (keyword rel) :curies) ; "curies" is reserved
          (every? link-properties (map keyword (apply map first links)))
          (every? :href links)]}
   nil)
@@ -38,23 +45,36 @@
 (defn add-curie
   "Adds a reference to the 'curies' section."
   [resource name href & property-pairs]
-  {:pre [(every? link-properties (map keyword (take-nth 2 property-pairs)))
+  {:pre [(is-resource resource)
+         (every? link-properties (map keyword (take-nth 2 property-pairs)))
          (even? (count property-pairs))
          (.contains href "{rel}")]}
   nil)
 
+
+(defn add-property-map
+  "Merges property map into the resource. Overwrites with new."
+  [resource m]
+  {:pre [(is-resource resource)
+         (not-any? reserved-resource-properties (map keyword (keys m)))]}
+  (merge resource m))
+
 (defn add-properties
   "Adds the specified properties to the resource."
   [resource & property-pairs]
-  {:pre [(not-any? reserved-resource-properties 
+  {:pre [(is-resource resource)
+         (not-any? reserved-resource-properties 
                    (map keyword (take-nth 2 property-pairs)))
          (even? (count property-pairs))]}
-  nil)
+  (add-property-map resource (apply hash-map property-pairs)))
 
-(defn add-embedded
+(defn add-embedded-set
   "Adds the specified embedded resources to the resource, with the name of the
   grouping as rel and contents as args. If there already exists a rel, adds the
   args to the existing rel."
-  [resource rel & resources]
-  {:pre [(every? #(-> % :_links :self :href) resources)]}
-  nil)
+  [resource rel resources]
+  {:pre [(is-resource resource)
+         (every? #(-> % :_links :self :href) resources)]}
+  (update-in resource
+             [:_embedded (keyword rel)]
+             #(apply (fnil conj []) % resources)))
--- wysitutwyg/src-clj/wysitutwyg/routes/corpora.clj
+++ wysitutwyg/src-clj/wysitutwyg/routes/corpora.clj
@@ -2,9 +2,13 @@
   (:import java.io.File)
   (:use [ring.util.response :only [response]])
   (:require [compojure.core :as compojure]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [wysitutwyg.hal.hal-core :as hal]))
 
 ;;;; Returns info on all corpora.
+;;;;   A lot of placeholders here since at some point I will move away from 
+;;;; this directory-as-database structure and put in a Proper Database, from
+;;;; which things like the size, description, etc, can be stored and retrieved.
 
 (def root (File. "resources/public/corpora"))
 (def corpora-names
@@ -21,13 +25,41 @@
        (slurp)
        (json/read-str)))
 
-(def all-parses (map get-parse-info corpora-names))
+(def all-parses 
+  (->> corpora-names
+       (map #(hash-map % (get-parse-info %)))
+       (apply merge)))
 
-(def rest-response
-  {:_embedded all-parses
-   :_links {:self "link"}})
+(def corpora-embedded
+  (->> corpora-names
+       (map #(-> (hal/new-resource (str "/corpora/" %))
+                 (hal/add-properties :name % 
+                                     :size "Placeholder"
+                                     :description "Placeholder")))))
+
+(def hal-all-corpora
+  (-> (hal/new-resource "/corpora")
+      (hal/add-properties :num-corpora (count corpora-embedded))
+      (hal/add-embedded-set "corpora" corpora-embedded)))
+
+; Awkward!
+(defn get-parses
+  [name]
+  (let [parses ((all-parses name) "targets")]
+    ;; Bad: self here isn't, uh, self, it's the json parse.
+    (->> (map #(str "/corpora/" name "/" (% "outfile")) parses)
+         (map hal/new-resource)
+         (map #(hal/add-property-map %2 %1) parses)))) ; This is awkward
+
+(defn hal-corpus
+  [name]
+  (if ((into #{} corpora-names) name)
+      (-> (hal/new-resource (str "/corpus/" name))
+          (hal/add-embedded-set :parses (get-parses name))
+          (response))
+      {:status 404 :body (str "No such corpus, " name)}))
 
 (compojure/defroutes routes
   (compojure/context "/corpora" []
-    (compojure/GET "/" [] (response rest-response))
-    (compojure/GET "/:id" [id] (response all-parses))))
+    (compojure/GET "/" [] (response hal-all-corpora))
+    (compojure/GET "/:name" [name] (hal-corpus name))))
